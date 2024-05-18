69b021126ac12cc06861b36eaed2cde090980f3b
Kate Ulloa-James <kate.ulloajames@gmail.com>
2024-05-18 20:18:10 +0000

    automated tests added

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,19 +1,13 @@
 from django.test import TestCase
 from .forms import ItemForm
+from .models import Item
 
-
-class TestDjango(TestCase):
+class TestModels(TestCase):
     
-    def test_item_name_is_required(self):
-        form = ItemForm({'name': ''})
-        self.assertFalse(form.is_valid())
-        self.assertIn('name', form.errors.keys())
-        self.assertEqual(form.errors['name'][0], 'This field is required')
+    def test_done_defaults_to_false(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertFalse(item.done)
         
-    def test_done_field_is_not_required(self):
-        form = ItemForm({'name': 'Test Todo Item'})
-        self.assertTrue(form.is_valid())
-    
-    def test_fields_are_explicit_in_form_metaclass(self):
-        form = ItemForm()
-        self.assertEqual(form.Meta.fields, ['name', 'done'])
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertEqual(str(item), 'Test Todo Item')
